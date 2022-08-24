cd3f1bd20601452faadc0786a02b574ee58a47fd
Fahad Hafiz <fahadhafiz1998@gmail.com>
2022-08-25 00:17:52 +0100

    Development[TASK] Added props to itemList

--- weather-app/App.js
+++ weather-app/App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ListItems from './components/ListItems';
 
+import { SAMPLE_DATA } from './assets/data/sampleData';
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -10,9 +12,33 @@ export default function App() {
       </View>
       <View style={styles.divider} />
 
-      <ListItems />
-      <ListItems />
-      <ListItems />
+      <ListItems
+          time_24h={SAMPLE_DATA[0].hourly.time[0]}
+          weather_id='0'
+          temp={SAMPLE_DATA[0].hourly.temperature_2m[0]}
+          precip='0'
+          wind_speed='0'
+          wind_rotation='60'
+          gust_speed='0' />
+
+      <ListItems
+          time_24h={SAMPLE_DATA[0].hourly.time[1]}
+          weather_id='0'
+          temp={SAMPLE_DATA[0].hourly.temperature_2m[1]}
+          precip='0'
+          wind_speed='0'
+          wind_rotation='180'
+          gust_speed='0' />
+
+      <ListItems
+          time_24h={SAMPLE_DATA[0].hourly.time[2]}
+          weather_id='0'
+          temp={SAMPLE_DATA[0].hourly.temperature_2m[2]}
+          precip='0'
+          wind_speed='0'
+          wind_rotation='270'
+          gust_speed='0' />
+
     </View>
   );
 }
--- weather-app/components/ListItems.js
+++ weather-app/components/ListItems.js
@@ -1,24 +1,30 @@
 import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 
-const ListItems = () => {
+const ListItems = ({time_24h, weather_id, temp, precip, wind_speed, wind_rotation, gust_speed}) => {
+  const rotate_wind = {
+    transform: [{
+      rotate: wind_rotation + 'deg',
+    }]
+  };
+
   return (
     <TouchableOpacity>
       <View style={styles.itemWrapper}>
         <View style={styles.itemText}>
-          <Text>22: 00</Text>
+          <Text>{time_24h}</Text>
         </View>
         <View style={styles.image}>
           <Image source={{uri: "https://cdn-icons-png.flaticon.com/512/1163/1163662.png" }} style={styles.image} />
         </View>
         <View style={styles.itemText}>
-          <Text>19&#8451;</Text>
+          <Text>{temp}&#8451;</Text>
         </View>
         <View style={styles.itemText}>
-          <Text>7&#x25;</Text>
+          <Text>{precip}&#x25;</Text>
         </View>
         <View style={styles.image}>
-          <Image source={{uri: "https://cdn-icons-png.flaticon.com/512/4005/4005837.png" }} style={[styles.image, styles.windDirection]} />
+          <Image source={{uri: "https://cdn-icons-png.flaticon.com/512/4005/4005837.png" }}  style={[styles.image, rotate_wind,]} />
         </View>
         <View style={styles.image}>
           <Image source={{uri: "https://cdn-icons-png.flaticon.com/512/6975/6975223.png" }} style={styles.image} />
@@ -45,11 +51,6 @@ const styles = StyleSheet.create({
     height: 48,
     width: 48,
   },
-
-  windDirection: {
-    transform: [{ rotate: '0deg' }]
-  },
-
 })
 
 export default ListItems
